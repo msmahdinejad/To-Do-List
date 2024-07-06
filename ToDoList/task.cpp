94b6f07eac0c989d83a121afdb44b275d2724c22
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-07-07 02:35:38 +0330

    task class completed!

--- ToDoList/task.cpp
+++ ToDoList/task.cpp
@@ -6,9 +6,72 @@ Task::Task(QWidget *parent)
     , ui(new Ui::Task)
 {
     ui->setupUi(this);
+    DB = QSqlDatabase::addDatabase("QSQLITE");
+    DB.setDatabaseName("C:/saleh/UNI/AP/To Do List/uiap-final-project-msmahdinejad/Data.db");
+    if(!DB.open())
+        qDebug() << "DB in Task page failed!";
+    else
+        qDebug() << "DB in Task page opened!";\
+
 }
 
 Task::~Task()
 {
     delete ui;
 }
+
+void Task::on_check_stateChanged(int s)
+{
+    if(s == Qt::Checked)
+    {
+        ui->name->setStyleSheet("QLabel { text-decoration: line-through; color: gray;}");
+        check = 1;
+        QSqlQuery query;
+        query.prepare("UPDATE task SET check = ? WHERE ID = ?");
+        query.addBindValue(check);
+        query.addBindValue(ID);
+        if (!query.exec()) {
+            qDebug() << "Error updating check: " << query.lastError();
+        }
+    }
+    else
+    {
+        ui->name->setStyleSheet("QLabel { text-decoration: none; color: black;}");
+        check = 0;
+        QSqlQuery query;
+        query.prepare("UPDATE task SET check = ? WHERE ID = ?");
+        query.addBindValue(check);
+        query.addBindValue(ID);
+        if (!query.exec()) {
+            qDebug() << "Error updating check: " << query.lastError();
+        }
+    }
+}
+
+
+void Task::on_star_stateChanged(int s)
+{
+    if(s == Qt::Checked)
+    {
+        star = 1;
+        QSqlQuery query;
+        query.prepare("UPDATE task SET star = ? WHERE ID = ?");
+        query.addBindValue(star);
+        query.addBindValue(ID);
+        if (!query.exec()) {
+            qDebug() << "Error updating star: " << query.lastError();
+        }
+    }
+    else
+    {
+        star = 0;
+        QSqlQuery query;
+        query.prepare("UPDATE task SET star = ? WHERE ID = ?");
+        query.addBindValue(star);
+        query.addBindValue(ID);
+        if (!query.exec()) {
+            qDebug() << "Error updating star: " << query.lastError();
+        }
+    }
+}
+
--- ToDoList/task.h
+++ ToDoList/task.h
@@ -11,9 +11,24 @@ class Task : public QWidget
 {
     Q_OBJECT
 
+private:
+    int ID;
+    QString user;
+    QString list;
+    QString name;
+    int check;
+    int star;
+    QString caption;
+    int assignCheck;
+    QString assignUser;
+    int year;
+    int month;
+    int day;
+
 public:
     explicit Task(QWidget *parent = nullptr);
     ~Task();
+
 
 private:
     Ui::Task *ui;
